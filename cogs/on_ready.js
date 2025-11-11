47d63ddad8472fc2b41b8362c580c937e4be527b
hugohugo130 <chan681976482@gmail.com>
2025-11-11 19:42:16 +0800

    feat: show startup time

--- cogs/on_ready.js
+++ cogs/on_ready.js
@@ -1,4 +1,4 @@
-const { Events } = require("discord.js");
+const { Events, Client, ActivityType } = require("discord.js");
 const { get_logger } = require("../utils/logger.js");
 const { checkDBFilesDefault } = require("../utils/check_db_files.js");
 const { run_schedule } = require("../utils/run_schedule.js");
@@ -7,6 +7,10 @@ const { BotName, authorName } = require("../utils/config.js");
 module.exports = {
     name: Events.ClientReady,
     once: true,
+    /**
+     * 
+     * @param {Client} client 
+     */
     execute: async function (client) {
         global._client = client;
         client.name = BotName || client.user.tag;
@@ -18,6 +22,14 @@ module.exports = {
         logger.info(`已加載 ${schedules} 個排程`);
 
         logger.info(`機器人 ${client.name} 啟動成功`);
-        logger.info(`好欸！已經有${client.guilds.cache.size}個伺服器在使用${client.name}了！`)
+        logger.info(`好欸！已經有${client.guilds.cache.size}個伺服器在使用${client.name}了！`);
+        client.user.setPresence({
+            activities: [
+                {
+                    name: `啟動時間: ${new Date().toLocaleString()}`,
+                    type: ActivityType.Custom,
+                },
+            ],
+        });
     },
 }
