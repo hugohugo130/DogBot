5a0f0c68500216592f3f94c7aa0643cf9cc8700a
hugohugo130 <chan681976482@gmail.com>
2025-11-11 22:29:20 +0800

    fix: /farm plant limit err

--- slashcmd/game/rpg/farm.js
+++ slashcmd/game/rpg/farm.js
@@ -228,7 +228,7 @@ module.exports = {
             const duration = 20 * 60;
             const endsAt = DateNowSecond() + duration;
 
-            if (farm_data.farms.length + amount >= farm_slots) {
+            if (farm_data.farms.length + amount > farm_slots) {
                 const embed = new EmbedBuilder()
                     .setColor(embed_error_color)
                     .setTitle(`${emoji_cross} | 最多只能同時使用四把鋤頭`);
