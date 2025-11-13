f5e8e994dfd2295f1a3a389e1d3c056daf62ef31
hugohugo130 <chan681976482@gmail.com>
2025-11-13 17:42:29 +0800

    fix: deprecated require

--- slashcmd/game/rpg/smelt.js
+++ slashcmd/game/rpg/smelt.js
@@ -97,8 +97,8 @@ module.exports = {
         if (subcommand === "smelt") {
             const { load_rpg_data, load_smelt_data } = require("../../../utils/file.js");
             const { notEnoughItemEmbed, name, smelter_slots, smeltable_items } = require("../../../utils/rpg.js");
-            const { setEmbedFooter, get_loophole_embed } = require("../../../cogs/rpg/msg_handler.js");
-            const { get_emoji } = require("../../../utils/rpg.js");
+            const { setEmbedFooter } = require("../../../cogs/rpg/msg_handler.js");
+            const { get_emoji, get_loophole_embed } = require("../../../utils/rpg.js");
             const { embed_error_color } = require("../../../utils/config.js");
 
             await interaction.deferReply();
