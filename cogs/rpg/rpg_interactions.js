e8a5ddc5728a9c42d5ec0452f75c28137379553f
hugohugo130 <chan681976482@gmail.com>
2025-11-13 18:31:08 +0800

    fix: /smelt smelt | confirm

--- cogs/rpg/rpg_interactions.js
+++ cogs/rpg/rpg_interactions.js
@@ -866,7 +866,7 @@ module.exports = {
 
                     if (!userHaveEnoughItems(userId, current_item_id, need_amount)) {
                         item_missing.push({
-                            name: get_name_of_id(current_item_id),
+                            item: get_name_of_id(current_item_id),
                             amount: need_amount - have_amount,
                         });
                     };
