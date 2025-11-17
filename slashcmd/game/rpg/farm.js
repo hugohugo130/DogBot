7961f7ffb8985980f0c0f3155fb15ab85d0018dd
hugohugo130 <chan681976482@gmail.com>
2025-11-17 09:55:42 +0800

    fix: bonus

--- slashcmd/game/rpg/farm.js
+++ slashcmd/game/rpg/farm.js
@@ -236,7 +236,7 @@ module.exports = {
             const duration = 20 * 60 - duration_deduction;
             const endsAt = DateNowSecond() + duration;
 
-            if ((farm_data.farms.length + insert_amount) > farm_slots) {
+            if ((farm_data.farms.length + insert_amount) > farm_slots + 10) {
                 const embed = new EmbedBuilder()
                     .setColor(embed_error_color)
                     .setTitle(`${emoji_cross} | 最多只能同時使用四把鋤頭`);
