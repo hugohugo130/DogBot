8c6446ff7fac2f0434152273f9963ce35292f22f
hugohugo130 <chan681976482@gmail.com>
2025-11-18 16:51:36 +0800

    rm: unnecessary print

--- utils/load_cogs.js
+++ utils/load_cogs.js
@@ -11,7 +11,6 @@ function load_cog(client, cog, itemPath) {
             await cog.execute(client, ...args);
         } catch (err) {
             logger.error(`執行 ${itemPath} 時發生錯誤: ${err.stack}`);
-            console.error(`執行 ${itemPath} 時發生錯誤: ${err.stack}`);
         };
     };
 
