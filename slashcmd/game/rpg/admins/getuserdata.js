d6625fcef449f35dec29315284a2cdd824d1e3f5
hugohugo130 <chan681976482@gmail.com>
2025-11-21 18:57:40 +0800

    feat: getuserdata cmd

---
+++ slashcmd/game/rpg/admins/getuserdata.js
@@ -0,0 +1,76 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("getuserdata")
+        .setDescription("Get rpg data of a user")
+        .setNameLocalizations({
+            "zh-CN": "获取用户数据",
+            "zh-TW": "獲取用戶數據",
+        })
+        .setDescriptionLocalizations({
+            "zh-CN": "获取用户的RPG数据",
+            "zh-TW": "獲取用戶的RPG數據",
+        })
+        .addUserOption(option =>
+            option.setName("user")
+                .setDescription("The user to get data from")
+                .setNameLocalizations({
+                    "zh-CN": "用户",
+                    "zh-TW": "用戶",
+                })
+                .setDescriptionLocalizations({
+                    "zh-CN": "要获取数据的用户",
+                    "zh-TW": "要獲取數據的用戶",
+                })
+                .setRequired(true),
+        )
+        .setDefaultMemberPermissions(0), // 只有管理員可以使用這個指令
+    async execute(interaction) {
+        const { load_rpg_data } = require("../../../../utils/file.js");
+        const { embed_default_color } = require("../../../../utils/config.js");
+        await interaction.deferReply();
+        const user = interaction.options.getUser("user");
+        const rpg_data = load_rpg_data(user.id);
+
+        /*
+            "money": 1000,
+            "hunger": 20,
+            "job": null,
+            "fightjob": null,
+            "badge": null,
+            "marry": {
+                "status": false,
+                "with": null,
+                "time": 0,
+            },
+            "lastRunTimestamp": {},
+            "inventory": {},
+            "transactions": [],
+            "count": {},
+            "privacy": [],
+        */
+
+        const embed = new EmbedBuilder()
+            .setColor(embed_default_color)
+            .setTitle(`${user.username}的RPG數據`)
+            .addFields(
+                { name: "金錢", value: `${rpg_data.money}`, inline: true },
+                { name: "飽食度", value: `${rpg_data.hunger}`, inline: true },
+                { name: "工作", value: `${rpg_data.job}`, inline: true },
+                { name: "戰鬥工作", value: `${rpg_data.fightjob}`, inline: true },
+                { name: "徽章", value: `${rpg_data.badge}`, inline: true },
+                { name: "結婚狀態", value: `${rpg_data.marry.status}`, inline: true },
+                { name: "結婚對象", value: `${rpg_data.marry.with}`, inline: true },
+                { name: "結婚時間", value: `${rpg_data.marry.time}`, inline: true },
+                { name: "物品", value: `${rpg_data.inventory}`, inline: false },
+                { name: "交易", value: `${rpg_data.transactions}`, inline: true },
+                { name: "隱私", value: `${rpg_data.privacy}`, inline: true },
+                { name: "上次執行時間", value: `${Object.entries(rpg_data.lastRunTimestamp).map(([key, value]) => `${key}: ${value}`).join("\n") || rpg_data.lastRunTimestamp}`, inline: false },
+                { name: "計數", value: `${Object.entries(rpg_data.count).map(([key, value]) => `${key}: ${value}`).join("\n") || rpg_data.count}`, inline: false },
+            )
+            .setTimestamp();
+
+        await interaction.editReply({ embeds: [embed] });
+    },
+};
