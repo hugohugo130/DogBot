d4e7c7529dc7246ed5d9cd43e19c607c86c604dd
hugohugo130 <chan681976482@gmail.com>
2025-11-24 19:15:27 +0800

    fix: safeshutdown | closeDatabase

--- utils/safeshutdown.js
+++ utils/safeshutdown.js
@@ -17,9 +17,9 @@ async function safeshutdown(client) {
             logger.info(`🛑 ${client.name || BotName || client.user.tag} 已關機！`);
         };
 
-        await shutdown(true, 200);
-
         closeDatabase();
+
+        await shutdown(true, 200);
     } finally {
         if (client?.destroy) await client.destroy();
         process.exit();
