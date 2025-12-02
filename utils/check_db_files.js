a7e21d3dc47e304edabda89816214a35e9195e9a
hugohugo130 <chan681976482@gmail.com>
2025-12-02 17:30:11 +0800

    feat: make db compatible when start bot

--- utils/check_db_files.js
+++ utils/check_db_files.js
@@ -1,10 +1,9 @@
-const { readJson, writeJson, existsSync, basename, join_db_folder } = require("./file.js");
+const { writeJson, existsSync, basename, join_db_folder, load_rpg_data } = require("./file.js");
 const { DATABASE_FILES, DEFAULT_VALUES, priorityUserIDs, priorityGuildIDs } = require("./config.js");
 const { get_logger } = require("./logger.js");
 const { wait_until_ready, client_ready } = require("./wait_until_ready.js");
 const { getDatabase } = require("./SQLdatabase.js");
-const fs = require("fs");
-const path = require("path");
+const { User } = require("discord.js");
 
 const logger = get_logger();
 const logger_nodc = get_logger({ nodc: true });
@@ -48,6 +47,49 @@ async function checkDBFilesCorrupted() {
     };
 
     if (err) process.exit(1);
+};
+
+/**
+ * 
+ * @param {Array<User>} users 
+ * @returns {void}
+ */
+function make_db_compatible(users) {
+    for (const user of users) {
+        if (user instanceof User) {
+            /*
+            2025 12 02:
+            - 把db.count的mine, hew, herd, brew, fish, fell，合併成work，並刪除
+            - 增加work鍵
+            */
+
+            const rpg_data = load_rpg_data(user.id);
+            if (!rpg_data.count) rpg_data.count = {};
+
+            const keys = ['mine', 'hew', 'herd', 'brew', 'fish', 'fell'];
+            for (const key of keys) {
+                if (!rpg_data.count.work) {
+                    rpg_data.count.work = 0
+                };
+
+                if (typeof rpg_data.count.work === "string") {
+                    logger.warn(`[userId ${user.id}] rpg_data.count.work is string, converted to number: ${rpg_data.count.work}`);
+
+                    rpg_data.count.work = parseInt(rpg_data.count.work);
+                };
+
+                if (rpg_data.count.work[key]) {
+                    if (typeof rpg_data.count.work[key] === "number") {
+                        rpg_data.count.work += rpg_data.count.work[key];
+                    };
+
+                    delete rpg_data.count.work[key];
+                };
+            };
+
+            save_rpg_data(user.id, rpg_data);
+        };
+    };
 };
 
 /**
@@ -122,6 +164,12 @@ async function checkDBFilesDefault(client) {
     for (const guild of guilds) {
         insertGuildStmt.run(guild.id);
     };
+
+    // === 關閉資料庫 ===
+    db.close();
+
+    // 執行 make_db_compatible
+    make_db_compatible();
 };
 
 module.exports = {
