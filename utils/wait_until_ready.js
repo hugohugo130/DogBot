4ef156573cd716375acb945649e6df4c96755973
hugohugo130 <chan681976482@gmail.com>
2025-12-07 21:39:21 +0800

    optimize: wait_until_ready

--- utils/wait_until_ready.js
+++ utils/wait_until_ready.js
@@ -1,8 +1,37 @@
+const { Client } = require("discord.js");
+const { get_logger, getCallerModuleName } = require("./logger");
+
+const logger = get_logger();
+
 function client_ready(client = global._client) {
     return client?.isReady?.();
 };
 
+function wait_for_client(waitReady = true, timeout = 10) {
+    const client = global._client;
+    const start = Date.now() / 1000;
+
+    while (true) {
+        if (start + timeout >= Date.now()) break;
+
+        if (waitReady) {
+            if (client_ready(client)) {
+                break;
+            };
+        } else if (client) {
+            break;
+        };
+    };
+
+    return client;
+};
+
 function wait_until_ready(client = global._client, timeout = 10) {
+    if (!client instanceof Client) {
+        logger.warn(`收到的client並不是Client Object, called from \n${getCallerModuleName(null)}`);
+        client = wait_for_client();
+    };
+
     if (!client) client = global._client;
     const start = Date.now() / 1000;
 
@@ -15,5 +44,6 @@ function wait_until_ready(client = global._client, timeout = 10) {
 
 module.exports = {
     client_ready,
+    wait_for_client,
     wait_until_ready,
 }
