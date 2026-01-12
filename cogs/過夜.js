523da138a4fbf9373563dea36f49d44597bc7e23
hugohugo130 <chan681976482@gmail.com>
2026-01-12 12:23:29 +0800

    optimize: auto join voice channel

--- cogs/過夜.js
+++ cogs/過夜.js
@@ -1,5 +1,5 @@
 const { Events } = require("discord.js");
-const { joinVoiceChannel } = require("@discordjs/voice");
+const { joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
 
 const { get_logger } = require("../utils/logger.js");
 const DogClient = require("../utils/customs/client.js");
@@ -20,13 +20,16 @@ module.exports = {
         const voiceChannel = guild.channels.cache.get("1456132424042807427");
         if (!voiceChannel) return;
 
-        joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: guild.id,
-            selfDeaf: true,
-            selfMute: false,
-            adapterCreator: guild.voiceAdapterCreator,
-        });
+        if (!getVoiceConnection("1422545977226690683")) {
+            joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: guild.id,
+                selfDeaf: true,
+                selfMute: false,
+                adapterCreator: guild.voiceAdapterCreator,
+            });
+        };
+
         logger.info(`✅ Joined voice channel ${voiceChannel.name}`);
     },
-}
+};
