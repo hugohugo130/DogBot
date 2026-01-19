5f910e3e9d418bceed7310cc463e731effe87971
hugohugo130 <chan681976482@gmail.com>
2026-01-19 22:23:27 +0800

    Update safeshutdown.js

--- utils/safeshutdown.js
+++ utils/safeshutdown.js
@@ -1,5 +1,5 @@
 const { uploadAllDatabaseFiles } = require("./onlineDB.js");
-const { get_logger, shutdown } = require("./logger.js");
+const { shutdown } = require("./logger.js");
 const { saveDvoiceData } = require("./file.js");
 const { BotName } = require("./config.js");
 const DogClient = require("./customs/client.js");
@@ -11,24 +11,21 @@ const DogClient = require("./customs/client.js");
 async function safeshutdown(client) {
     try {
         await saveDvoiceData(client.dvoice.toJSON() || {});
+        console.log("成功保存dvoice資料！");
 
-        const success = await uploadAllDatabaseFiles();
+        const [success, _] = await Promise.allSettled([
+            uploadAllDatabaseFiles(),
+            shutdown(true, 200),
+        ]);
 
-        if (client) {
-            const logger = get_logger();
-
-            logger.info(success ? "已上載所有資料庫檔案" : "上載資料庫檔案失敗，下次請選擇上載資料庫檔案或無操作！");
-            logger.info("成功保存dvoice資料！");
-            logger.info(`🛑 ${client.name || BotName || client.user.tag} 已關機！`);
-        };
-
-        await shutdown(true, 200);
+        console.log(success.status === "fulfilled" ? "已上載所有資料庫檔案" : `上載資料庫檔案失敗，下次請選擇上載資料庫檔案或無操作！\n${success.reason}`);
+        console.log(`🛑 ${client?.name || BotName || client?.user?.tag} 已關機！`);
     } finally {
-        if (client?.destroy) await client.destroy();
+        await client?.destroy?.();
         process.exit();
     };
 };
 
 module.exports = {
     safeshutdown,
-}
+};
