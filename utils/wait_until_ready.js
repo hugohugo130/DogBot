1713f4810ea6cccc5c07f69759640446bd58cfd3
hugohugo130 <chan681976482@gmail.com>
2026-01-20 21:45:22 +0800

    optimize: wait until ready

--- utils/wait_until_ready.js
+++ utils/wait_until_ready.js
@@ -1,41 +1,37 @@
 const { Client } = require("discord.js");
-const { get_logger } = require("./logger");
-
-const logger = get_logger();
+const { asleep } = require("./sleep");
 
 function client_ready(client = global._client) {
     return client?.isReady?.();
 };
 
-function wait_for_client(waitReady = true, timeout = 10) {
+async function wait_for_client(waitReady = true, timeout = 10000, wait = 500) {
     const client = global._client;
-    const start = Date.now() / 1000;
+    const start = Date.now();
 
     while (true) {
-        if (start + timeout >= Date.now()) break;
+        if ((start + timeout) >= Date.now()) break;
 
         if (waitReady) {
-            if (client_ready(client)) {
-                break;
-            };
-        } else if (client) {
-            break;
-        };
+            if (client_ready(client)) break;
+        } else if (client) break;
+
+        await asleep(wait);
     };
 
     return client;
 };
 
-function wait_until_ready(client = global._client, timeout = 10) {
-    if (!client instanceof Client) {
-        client = wait_for_client();
-    };
+async function wait_until_ready(client = global._client, timeout = 10000, wait = 500) {
+    if (!client instanceof Client) client = wait_for_client();
 
     if (!client) client = global._client;
-    const start = Date.now() / 1000;
+    const start = Date.now();
 
     while (true) {
-        if (client_ready(client) || start + timeout >= Date.now()) break;
+        if (client_ready(client) || (start + timeout) >= Date.now()) break;
+
+        await asleep(wait);
     };
 
     return client_ready(client) ? client : null;
@@ -45,4 +41,4 @@ module.exports = {
     client_ready,
     wait_for_client,
     wait_until_ready,
-}
+};
