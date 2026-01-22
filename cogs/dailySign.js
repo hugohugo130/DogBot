673e9bbd37d880acaf45a3e9a2c7ca78de2c0e23
hugohugo130 <chan681976482@gmail.com>
2026-01-22 17:36:58 +0800

    fix: daily sign

--- cogs/dailySign.js
+++ cogs/dailySign.js
@@ -4,6 +4,7 @@ const { load_rpg_data, save_rpg_data } = require("../utils/file.js");
 const { daily_sign_guildIDs } = require("../utils/config.js");
 const DogClient = require("../utils/customs/client.js");
 const { add_money } = require("../utils/rpg.js");
+const { randint } = require("../utils/random.js");
 
 module.exports = {
     name: Events.MessageCreate,
@@ -15,15 +16,22 @@ module.exports = {
     execute: async function (client, message) {
         const { author: user, guild } = message;
 
-        if (user.bot || !guild) return;
-        if (!daily_sign_guildIDs.includes(guild.id)) return;
+        if (user.bot || !guild || !daily_sign_guildIDs.includes(guild.id)) return;
 
         const rpg_data = await load_rpg_data(user.id);
         if (!rpg_data.daily) rpg_data.daily = 0;
+        if (!rpg_data.daily_times) rpg_data.daily_times = 0;
+
+        const daily_times = rpg_data.daily_times;
 
         const now = new Date();
         const last_sign = new Date(rpg_data.daily);
         if (now.getDate() === last_sign.getDate()) return;
+
+        const money_per_dt = randint(5, 10);
+        const bonus = money_per_dt * daily_times;
+
+        const amount = randint(150 + bonus, 500);
 
         rpg_data.money = add_money({
             rpg_data,
