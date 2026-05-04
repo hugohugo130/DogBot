281960fd80a8e77e20f28d639fb4ba925a03ceb2
hugohugo130 <chan681976482@gmail.com>
2026-05-04 17:32:22 +0800

    fix: rpg db fixer

--- schedule/every5min/fix_user_rpg_database.js
+++ schedule/every5min/fix_user_rpg_database.js
@@ -12,49 +12,64 @@ module.exports = {
      * @param {DogClient} client 
      */
     execute: async function (client) {
+        return;
+
         try {
-            // const users = client.users.cache.values();
-            // for (const user of users) {
-            //     if (user.bot) continue;
+            const users = client.users.cache.values();
+            for (const user of users) {
+                if (user.bot) continue;
 
-            //     const rpg_data = await load_rpg_data(user.id);
+                const rpg_data = await load_rpg_data(user.id);
 
-            //     if (!rpg_data) continue;
+                if (!rpg_data) continue;
 
-            //     const backpack = rpg_data.backpack || {};
-            //     let modified = false;
+                const backpack =
+                    /** @type {{ [k: string]: any }} */
+                    (rpg_data.inventory)
+                    || {};
 
-            //     for (const [item, amount] of Object.entries(backpack)) {
-            //         if (typeof amount === "string") {
-            //             logger.warn(`用戶 ${user.id} 的背包中 ${item} 的數量是字符串，正在修復... (${amount})`)
+                let modified = false;
 
-            //             try {
-            //                 backpack[item] = parseInt(amount);
+                for (const [item, amount] of Object.entries(backpack)) {
+                    if (typeof amount === "string") {
+                        logger.debug(`用戶 ${user.id} 的背包中 ${item} 的數量是字符串，正在修復... (${amount})`)
 
-            //                 modified = true;
-            //             } catch {
-            //                 // 過濾掉非數字的字符
-            //                 const filteredAmount = amount.replace(/[^0-9]/g, "");
-            //                 backpack[item] = parseInt(filteredAmount) || 0;
+                        try {
+                            // 過濾掉非數字的字符
+                            const filteredAmount = amount.replace(/[^0-9]/g, "");
 
-            //                 modified = true;
-            //             };
-            //         };
+                            backpack[item] = parseInt(filteredAmount);
 
-            //         if (amount === 0) {
-            //             logger.warn(`用戶 ${user.id} 的背包中 ${item} 的數量為0，正在移除...`)
-            //             delete backpack[item];
-            //             modified = true;
-            //         };
-            //     };
+                            modified = true;
+                        } catch {
+                            delete backpack[item];
 
-            //     if (modified) {
-            //         rpg_data.backpack = backpack;
+                            modified = true;
+                        };
+                    };
 
-            //         await save_rpg_data(user.id, rpg_data);
-            //         logger.warn(`已修復用戶 ${user.id} 的背包資料`);
-            //     };
-            // };
+                    if (amount === 0) {
+                        logger.debug(`用戶 ${user.id} 的背包中 ${item} 的數量為0，正在移除...`)
+                        delete backpack[item];
+                        modified = true;
+                    };
+
+                    if (typeof amount === "number" && !Number.isInteger(amount)) {
+                        logger.debug(`用戶 ${user.id} 的背包中 ${item} 的數量為小數，轉換為整數`)
+                        backpack[item] = Math.floor(amount);
+
+                        modified = true;
+                    };
+
+                };
+
+                if (modified) {
+                    rpg_data.inventory = backpack;
+
+                    await save_rpg_data(user.id, rpg_data);
+                    logger.info(`已修復用戶 ${user.id} 的背包資料`);
+                };
+            };
         } catch (error) {
             const errorStack = util.inspect(error, { depth: null });
 
