383d63cf2e75b164024d5dea2283027709b9812a
hugohugo130 <chan681976482@gmail.com>
2026-05-04 18:04:12 +0800

    deprecated: utils/senderr.js

--- utils/senderr.js
+++
@@ -1,9 +0,0 @@
-const { get_logger, getCallerModuleName } = require("./logger.js");
-
-const logger = get_logger();
-
-module.exports = {
-    senderr() {
-        logger.warn(`[DEPRECATED] senderr called from ${getCallerModuleName()}`);
-    },
-};
