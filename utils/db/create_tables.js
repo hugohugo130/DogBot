e9099bdd7c881e85dca4d1c0e3c0f6f7391b3aa7
hugohugo130 <chan681976482@gmail.com>
2026-08-15 11:44:11 +0800

    feat(db): auto create tables

---
+++ utils/db/create_tables.js
@@ -0,0 +1,39 @@
+import { connectPool } from "./db.js";
+import { get_logger } from "../logger.js";
+import { importModules } from "../customs/custom_import.js";
+
+export async function create_tables(cache = true) {
+    const logger = get_logger();
+    const { TABLES_METADATA } =
+        /** @type {import("./config.js")} */
+        (await importModules("./config.js", cache));
+
+    const commands = Object.entries(TABLES_METADATA)
+        .filter(([_, value]) => value.CREATE !== undefined)
+        .map(([key, value]) => /** @type {[keyof typeof TABLES_METADATA, string]} */([key, (value.CREATE)]));
+
+    logger.info("正在檢查資料庫表是否存在");
+
+    for (const [table_name, command] of commands) {
+        const client = await connectPool();
+        try {
+            await client.init(table_name);
+
+            const exists = await client.is_table_exists();
+            if (exists) continue;
+
+            // 上面檢查的時候已經有begin過了
+            // await client.begin();
+
+            await client.query(command);
+            logger.info(`已創建資料庫表: ${table_name}`);
+        } catch (err) {
+            await client.rollback();
+            throw err;
+        } finally {
+            client.release();
+        };
+    };
+
+    logger.info("已檢查資料庫表是否存在");
+};
--- utils/db/db.js
+++ utils/db/db.js
@@ -1,7 +1,15 @@
 import { Pool, Client as PGClient } from "pg";
 import { importModules } from "../customs/custom_import.js";
 
+/**
+ * @typedef {Object} ClientInitOptions
+ * @property {keyof typeof import("./config.js").TABLES_METADATA} table_name
+ * @property {boolean} [cache=false]
+ */
+
 export class PoolClient extends PGClient {
+    #began;
+
     /**
      * @param {string | import("pg").ClientConfig} [config]
      */
@@ -10,13 +18,15 @@ export class PoolClient extends PGClient {
 
         /** @type {string | null} */
         this.table_name = null;
-        this._began = false;
+
+        /** @type {boolean} */
+        this.#began = false;
     };
 
     /**
      * @returns {asserts this is { table_name: string }}
      */
-    _check_table_name() {
+    #check_table_name() {
         if (!this.table_name) {
             throw new Error("Table name has not been set");
         };
@@ -39,19 +49,60 @@ export class PoolClient extends PGClient {
         this.table_name = table_name;
     };
 
-    async begin() {
+    /**
+     * @param {boolean} [force=false]
+     * @returns {Promise<void>}
+     */
+    async begin(force = false) {
+        if (!force && this.#began) return;
+
         await this.query("BEGIN");
-        this._began = true;
+        this.#began = true;
     };
 
     async commit() {
         await this.query("COMMIT");
-        this._began = false;
+        this.#began = false;
     };
 
     async rollback() {
         await this.query("ROLLBACK");
-        this._began = false;
+        this.#began = false;
+    };
+
+    /**
+     * @param {string | null} [table_name] - The table name to check, defaults to the name while initalizing
+     * @returns {Promise<boolean>} Whether the table exists
+     */
+    async is_table_exists(table_name = null) {
+        if (!table_name) {
+            this.#check_table_name();
+            table_name = this.table_name;
+        } else {
+            const { TABLES_METADATA } =
+                /** @type {import('./config.js')} */
+                (await importModules("./config.js", true));
+
+            if (!(table_name in TABLES_METADATA)) {
+                throw new Error("Table name must in the TABLES_METADATA");
+            };
+        };
+
+
+        /*
+        {
+            command: 'SELECT',
+            rowCount: 1,
+            oid: null,
+            rows: [ { table_exists: true } ], // <--- The result is here
+            fields: [ { name: 'table_exists', dataTypeID: 16 } ] // 16 is the OID for boolean
+        }
+        */
+        await this.begin();
+        const command = `SELECT to_regclass('${table_name}') IS NOT NULL AS table_exists`;
+        const result = await this.query(command);
+
+        return !!result.rows[0]?.["table_exists"];
     };
 
     /**
@@ -59,7 +110,7 @@ export class PoolClient extends PGClient {
      * @returns {Promise<any>}
      */
     async get(columns = "*") {
-        this._check_table_name();
+        this.#check_table_name();
 
         if (!Array.isArray(columns)) {
             columns = [columns];
@@ -67,8 +118,10 @@ export class PoolClient extends PGClient {
 
         const command = `SELECT ${columns.join(", ")} FROM ${this.table_name}`;
 
-        if (!this._began) this.begin();
-        return await this.query(command);
+        await this.begin();
+        const result = await this.query(command);
+
+        return result.rows;
     };
 
     /**
@@ -76,10 +129,11 @@ export class PoolClient extends PGClient {
      */
     release(err) {
         // Pool 會自動注入代碼
-    }
+    };
 };
 
 const _pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     Client: PoolClient,
 }); // .env需填寫 DATABASE_URL
 
